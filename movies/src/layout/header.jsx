e72455bf383b37754414e09a8cfd815e86f30212
Qeqley <prokopenkoolgas@gmail.com>
2025-04-06 15:21:31 +0300

    reinitialized files

---
+++ movies/src/layout/header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+    return <nav className="flat-pumpkin accent-1">
+    <div className="container">
+    <div className="nav-wrapper">
+      <a href="!#" className="brand-logo">React Movies</a>
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+      </ul>
+    </div>
+    </div>
+  </nav>
+}
+export { Header };
